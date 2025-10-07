b19683a9dfe7e41f35b08360738819760d9d72d6
Igor Melnikoc <zero_88@bk.ru>
2025-10-07 18:56:55 +0300

    replace obsolere OpenFileDialog usage

--- LiteDb.Studio.Avalonia/Views/ConnectionControl.axaml.cs
+++ LiteDb.Studio.Avalonia/Views/ConnectionControl.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 using LiteDb.Studio.Avalonia.ViewModels;
 
 namespace LiteDb.Studio.Avalonia.Views;
@@ -14,24 +15,22 @@ public partial class ConnectionControl : UserControl
         AvaloniaXamlLoader.Load(this);
     }
 
-    public ConnectionViewModel ViewModel => DataContext as ConnectionViewModel;
+    public ConnectionViewModel? ViewModel => DataContext as ConnectionViewModel;
 
     private async void SelectDbFileClick(object? sender, RoutedEventArgs e)
     {
         if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            var dialog = new OpenFileDialog
-            {
-                AllowMultiple = false,
-                Title = "Select the LiteDB file"
-            };
-
-            if (this.Parent is Panel parentPanel &&
-                parentPanel.Parent is Window window)
-            {
-                var files = await dialog.ShowAsync(window);
-                ViewModel?.Set(files);
-            }
+            var storageProvider = desktop.MainWindow!.StorageProvider;
+            var files = await storageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
+                {
+                    Title = "Select the LiteDB file",
+                    FileTypeFilter = [FilePickerFileTypes.All]
+                }
+            );
+            
+            var filePaths = files.Select(x => x.Path.LocalPath).ToArray();
+            ViewModel?.Set(filePaths);
         }
     }
 }
